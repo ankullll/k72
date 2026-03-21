652bfd1f72db4c6a7049cf2dc9b999fa0071f7a6
Ankullll <ankulsingh975@gmail.com>
2026-03-21 20:52:17 +0530

    fixed video element 7

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -2,7 +2,7 @@
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1774095094~exp=1774098694~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=39a6998aff37037ddf19bc9212e9366ec3495edbed9fa1d430efd1c2953ce4c8&r=dXMtZWFzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="../69496b2d.mp4"></video>
     </div>
   )
 }
